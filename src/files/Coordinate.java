e05706cb88ef47895203bdc574e5e2f3dac04a87
Filipv403 <64977122+Filipv403@users.noreply.github.com>
2020-05-16 14:12:51 +0200

    Add files via upload

--- src/files/Coordinate.java
+++ src/files/Coordinate.java
@@ -1,46 +1,93 @@
 package files;
 
 /**
+ * Třída pro souřadnice
+ * 
  * @author xvacla30
  */
 public class Coordinate{
 
+    /**
+     * souřadnice x a y
+     */
     private int x = 0;
     private int y = 0;
 
+    /**
+     * Prázdný konstruktor, nastaví hodnoty souřadnic na 0
+     */
     public Coordinate() {
         this.x = 0;
         this.y = 0;
     }
 
+    /**
+     * Konstruktor nastaví hodnoty souřadnic dle parametru
+     * 
+     * @param x souřadnice x
+     * @param y souřadnice y
+     */
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * 
+     * @return x souřadnici
+     */
     public int getX() {
         return this.x;
     }
 
+    /**
+     * 
+     * @return y souřadnici
+     */
     public int getY(){
         return this.y;
     }
 
+    /**
+     * Prázdné souřadnici nastaví souřadnice x a y
+     * 
+     * @param x souřadnice
+     * @param y souřadnice
+     */
     public void setXY(int x, int y){
         this.x = x;
         this.y = y;
     }
-    //rozdíl souřadnic
+    
+    /**
+     * Vypočítá rozdíl této souřadnici se zadanou souřadnicí na x ose
+     * 
+     * @param c odečítaná souřadnice
+     * @return rozdíl souřadnic na x ose
+     */
     public int diffX(Coordinate c){
         int diffX = this.x - c.x;
         return diffX;
     }
 
+    /**
+     * Vypočítá rozdíl této souřadnici se zadanou souřadnicí na y ose
+     * 
+     * @param c odečítaná souřadnice
+     * @return rozdíl souřadnic na y ose
+     */
     public int diffY(Coordinate c){
         int diffY = this.y - c.y;
         return diffY;
     }
 
+    /**
+     * Vytvoří novou souřadnici 
+     * 
+     * @param x souřadnice
+     * @param y souřadnice
+     * @return vytvořenou souřadnici
+     */
     public static Coordinate create(int x, int y){
         if(x < 0 || y < 0){
             return null;
